129fa5b12468728af8d23758c0d84314fcabcf69
lr1n <sko0hlol@gmail.com>
2021-07-30 19:46:49 +0200

    Added connection with PostgreSQL db

--- sibdev/settings.py
+++ sibdev/settings.py
@@ -77,9 +77,17 @@ WSGI_APPLICATION = 'sibdev.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sibdev_db',
+        'USER': 'sibdev',
+        'PASSWORD': 'sibdev',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
