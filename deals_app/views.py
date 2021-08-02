4b280960b4bc9d01adef6487a2192c4f11e6687c
lr1n <sko0hlol@gmail.com>
2021-08-02 18:33:35 +0200

    Added function that process a csv file

--- deals_app/views.py
+++ deals_app/views.py
@@ -1,8 +1,6 @@
 import csv
 import io
-import codecs
 from django.http.response import HttpResponseRedirect
-from django.contrib import messages
 from django.shortcuts import render
 from django.views.generic import ListView
 
@@ -10,29 +8,83 @@ from .forms import DealsForm
 from .models import DealsModel, DataFromDealsFiles
 
 
+def process_csv(filename):
+    customers = set()
+    data = dict()
+
+    with open(filename) as f:
+        reader = csv.reader(f, delimiter=',')
+        next(reader)
+        reader = list(reader)
+
+        for el in reader:
+            customers.add(el[0])
+
+        for el in customers:
+            data[el] = [0, set()]
+
+        for el in reader:
+            data[el[0]][0] += int(el[2])
+            data[el[0]][1].add(el[1])
+
+    sorted_data = {
+        k: v for k, v in sorted(data.items(), key=lambda x: x[1], reverse=True)
+    }
+
+    gems = []
+    data_5_items = []
+    for k, v in sorted_data.items():
+        data_5_items.append([k, v[0], v[1]])
+        gems.extend(list(v[1]))
+        if len(data_5_items) > 4:
+            break
+
+    for el in data_5_items:
+        for gem in gems:
+            if gem in el[2] and gems.count(gem) < 2:
+                el[2].remove(gem)
+
+    return data_5_items
+
+
 def add_deal(request):
     if request.method == 'POST':
-        csv_file = request.FILES['upload_deal']
-        data = csv_file.read().decode('utf-8')
-        io_string = io.StringIO(data)
-        next(io_string)
-        for c in csv.reader(io_string, delimiter=','):
-            _, created = DataFromDealsFiles.objects.update_or_create(
-                customer=c[0],
-                item=c[1],
-                total=c[2],
-                quantity=c[3],
-                date=c[4]
-            )
-        # file = form.cleaned_data['upload_deal']
-        # processed_csv_file = csv.reader(
-        #     codecs.iterdecode(file, 'utf-8')
-        # )
-        # next(processed_csv_file)
         form = DealsForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('index/')
+        file = request.FILES['upload_deal']
+        if file.name.endswith('.csv'):
+            data = file.read().decode('utf-8')
+            io_str = io.StringIO(data)
+            next(io_str)
+            reader = csv.reader(io_str, delimiter=',')
+            for el in reader:
+                d = DataFromDealsFiles(
+                    customer=el[0],
+                    item=el[1],
+                    total=el[2],
+                    quantity=el[3],
+                    date=el[4]
+                )
+                d.save()
+            # for c in csv.reader(io_str, delimiter=','):
+            #     _, created = DataFromDealsFiles.objects.update_or_create(
+            #         customer=c[0],
+            #         item=c[1],
+            #         total=c[2],
+            #         quantity=c[3],
+            #         date=c[4]
+            #     )
+
+            # file = form.cleaned_data['upload_deal']
+            # processed_csv_file = csv.reader(
+            #     codecs.iterdecode(file, 'utf-8')
+            # )
+            # next(processed_csv_file)
+            if form.is_valid():
+                form.save()
+        else:
+            if form.is_valid():
+                form.save()
+                return HttpResponseRedirect('index/')
     else:
         form = DealsForm()
     return render(request, 'deals_app/add_deal.html', {'form': form})
